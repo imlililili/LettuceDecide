a546ca0e0f082a455c3dcab6bb729fe9cf7414f1
LiYing Chen <78844463+imlililili@users.noreply.github.com>
2026-08-30 03:35:37 +1000

    Initial Commit

---
+++ LettuceDecide/LettuceDecideApp.swift
@@ -0,0 +1,17 @@
+//
+//  LettuceDecideApp.swift
+//  LettuceDecide
+//
+//  Created by Emily on 30/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct LettuceDecideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LettuceDecideTests/LettuceDecideTests.swift
@@ -0,0 +1,19 @@
+//
+//  LettuceDecideTests.swift
+//  LettuceDecideTests
+//
+//  Created by Emily on 30/8/2026.
+//
+
+import Testing
+@testable import LettuceDecide
+
+struct LettuceDecideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
